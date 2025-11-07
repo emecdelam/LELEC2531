be1968dc0cc90b5ee14567ff5938869503040215
emecdelam <kiwi.bp.surviv@gmail.com>
2025-11-07 10:03:48 +0100

    fixing wrongfile

--- HW2/hw2.sv
+++ HW2/hw2.sv
@@ -1,9 +1,10 @@
+//`timescale 1ns/1ps 	
 module Homework_2 (input logic clk,
 input logic reset,
 input logic [2:0] buttons,
 output logic [1:0] action);
 
-typedef enum logic [1:0] {S , J, D, R} statetype;
+typedef enum logic [1:0] {S, J, D, R} statetype;
 statetype state, nextstate;
 
 always_ff @(posedge clk)
@@ -13,33 +14,22 @@ always_ff @(posedge clk)
 	
 always_comb begin
 	case (state)
-		S: if (buttons[2])						nextstate = J;
-			else if (buttons[1]) 				nextstate = R;
-			else								nextstate = S;
-		R: if (buttons[2])						nextstate = J;
-			else if (buttons[1])				nextstate = R;
-			else								nextstate = S;
-		J: if ((buttons & 3'b101) == 3'b101)	nextstate = D;
-			else if (buttons[1])				nextstate = R;
-			else 								nextstate = S;
-		D: 										nextstate = S;
-		default:								nextstate = S;
+        S: if (buttons == 3'b100)			nextstate = J;
+        else if (buttons == 3'b010) 		nextstate = R;
+			else				nextstate = S;
+        R: if (buttons == 3'b100)			nextstate = J;
+        else if (buttons == 3'b010)		nextstate = R;
+			else				nextstate = S;
+        J: if (buttons == 3'b101)	nextstate = D;
+        else if (buttons == 3'b010)		nextstate = R;
+			else 				nextstate = S;
+		D: 					nextstate = S;
+		default:				nextstate = S;
 	endcase
 end
-
-always_comb begin
-	case (state)
-		S: action = 2'b00;
-		J: action = 2'b01;
-		D: action = 2'b10;
-		R: action = 2'b11;
-		default: action = 2'b00;
-	endcase
-end
+assign action = state;
 
 endmodule
-
-
 module testhw;
 	
 	// signals
